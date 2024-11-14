724fe5a06d7fca27dee8998813537d62f100eaf7
unknown <d.bhargavi280702@gmail.com>
2024-11-14 11:09:46 +0530

    string methods2 file is added

---
+++ str methods.js
@@ -0,0 +1,21 @@
+let str = "Hello World!";
+let newStr=str.substring(0,6)+"My Dear Friend";
+console.log(newStr);
+
+
+let a="Hungry";
+console.log(a.repeat(10));
+
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"));
+
+
+let message = "Hello, world!";
+let msg=message.startsWith("Hi");
+console.log(msg);
+
+
+let fileName = "document.pdf";
+let ends=".pdf"
+console.log(fileName.endsWith(ends));
